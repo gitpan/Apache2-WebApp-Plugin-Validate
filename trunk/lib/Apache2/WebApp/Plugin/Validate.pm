679b2a8850b8fefcf383fbb4d8d667658ff82364
Marc S. Brooks <mbrooks@cpan.org>
2010-01-02 07:35:08 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Validate-0.04 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Validate
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Validate-0.04.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- trunk/lib/Apache2/WebApp/Plugin/Validate.pm
+++ trunk/lib/Apache2/WebApp/Plugin/Validate.pm
@@ -25,7 +25,7 @@ use HTTP::BrowserDetect;
 use Net::DNS::Check;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
