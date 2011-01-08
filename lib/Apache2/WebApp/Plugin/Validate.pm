190c454d7605959304359e2e70d46b44e6d528e0
Marc S. Brooks <mbrooks@cpan.org>
2011-01-08 13:21:42 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Validate-0.08 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Validate
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Validate-0.08.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/Validate.pm
+++ lib/Apache2/WebApp/Plugin/Validate.pm
@@ -25,7 +25,7 @@ use HTTP::BrowserDetect;
 use Net::DNS::Check;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -378,6 +378,6 @@ Marc S. Brooks, E<lt>mbrooks@cpan.orgE<gt> - L<http://mbrooks.info>
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-See L<http://www.perl.com/perl/misc/Artistic.html>
+See L<http://dev.perl.org/licenses/artistic.html>
 
 =cut
